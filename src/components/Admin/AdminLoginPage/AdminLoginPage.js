9996fccda862014580e79edddc7bb0c58a8893a4
amal1231 <amalbpalackal@gmail.com>
2024-02-19 13:29:54 +0530

    Admin Login page done

---
+++ src/components/Admin/AdminLoginPage/AdminLoginPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../../NavBar/NavBar'
+import SideBar from '../../Home/SideBar/SideBarAdmin'
+
+function AdminLoginPage() {
+  return (
+    <div>
+      <Navbar/>
+      <SideBar/>
+    </div>
+  )
+}
+
+export default AdminLoginPage
---
+++ src/components/Home/SideBar/SideBarAdmin.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import 'boxicons';
+import './SideBarAdmin.css' 
+
+const SideBar = () => {
+  return (
+    <div className="sidebar close">
+      <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'></link>
+      <ul className="nav-links">
+        <li>
+          <div className="iocn-link">
+            <a href="/ViewPatientProfile">
+            <i class='bx bx-user-pin'></i>
+              <span className="link_name">View Patient Profile</span>
+            </a>
+          </div>
+          <ul className="sub-menu">
+            <li><a className="link_name" href="/ViewPatientProfile">View Patient Profile</a></li>
+          </ul>
+        </li>
+        <li>
+          <div className="iocn-link">
+            <a href="#"><i class='bx bx-user-plus'></i></a>
+          </div>
+          <ul className="sub-menu">
+            <li><a className="link_name" href="#">Add User</a></li>
+          </ul>
+        </li>
+
+        <li>
+          <a href="/GiveFeedback">
+            <i class='bx bx-message'></i>
+            <span className="link_name">Give Feedback</span>
+          </a>
+          <ul className="sub-menu blank">
+            <li><a className="link_name" href="/GiveFeedback">Give Feedback</a></li>
+          </ul>
+        </li>
+
+        <li>
+          <a href="/Login">
+            <i className='bx bx-log-out'></i>
+            <span className="link_name">Log Out</span>
+          </a>
+          <ul className="sub-menu blank">
+            <li><a className="link_name" href="/Logout">Log Out</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default SideBar;
